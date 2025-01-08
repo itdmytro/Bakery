6dbcfee310b7f10968f2c3a9c02113e740bdbac9
Dmytro Iliushchenko <105583659+itdmytro@users.noreply.github.com>
2025-01-08 16:08:30 +0200

    Continue adaptive, add 2x images

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,10 @@
 const modal = document.querySelector('.backdrop');
 const ModalOpen = document.querySelector('.js-modal-open');
+const ModalOpenMobile = document.querySelector('.js-modal-open-mobile');
 const ModalClose = document.querySelector('.js-modal-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
 ModalOpen.addEventListener('click', toggleModal);
+ModalOpenMobile.addEventListener('click', toggleModal);
 ModalClose.addEventListener('click', toggleModal);
