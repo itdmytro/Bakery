3a777536efcbcc59394942fffcb95126cce65596
Dmytro Iliushchenko <105583659+itdmytro@users.noreply.github.com>
2025-01-14 01:56:41 +0200

    Finish adaptive, add content in burger

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,8 +1,12 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const menuOpen = document.querySelector('.js-burger-open');
 const menuClose = document.querySelector('.js-burger-close');
+const body = document.querySelector('body');
 
 const toggleMenu = () => burgerMenu.classList.toggle('is-open');
+const toggleBody = () =>  body.classList.toggle('scroll-blocked');
 
 menuOpen.addEventListener('click', toggleMenu);
 menuClose.addEventListener('click', toggleMenu);
+menuOpen.addEventListener('click', toggleBody);
+menuClose.addEventListener('click', toggleBody);
